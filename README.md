d5fb95da3a616fd4eeece8083a25dcdc5ff87e26
jker <cxgmfy@163.com>
2021-11-10 09:31:26 +0800

    *支持删除操作 *修复冗余换行符导致原格式被显著破坏问题 *优化性能

--- README.md
+++ README.md
@@ -1,5 +1,91 @@
 # jini
 
+### 简介
+
+* 轻量
+* 有序
+* 增/删/改/查
+* 支持顶级节点
+* 键值支持多行
+* 尽可能不破坏原格式
+
+### 功能
+
+```
+// NewIni
+// @Description:加载INI文件
+// @param pFileName 文件名
+// @param pCreate 当文件不存在时,是否创建文件
+// @return rt
+// @return err
+func NewIni(pFileName string, pCreate bool) (rt *Ini, err error)
+
+// GetFileName
+// @Description: 获取当前文件名
+// @receiver i
+// @return string
+func (i *Ini) GetFileName() string
+
+// HasSection
+// @Description: 查询节点是否存在
+// @receiver i
+// @param pSection 节点名
+// @return bool
+func (i *Ini) HasSection(pSection string) bool
+
+// HasKey
+// @Description: 查询键是否存在
+// @receiver i
+// @param pSection 节点名
+// @param pKey 键名
+// @return bool
+func (i *Ini) HasKey(pSection string, pKey string) bool
+
+// DelSection
+// @Description: 删除节点
+// @receiver i
+// @param pSection 节点名
+func (i *Ini) DelSection(pSection string)
+
+// DelKey
+// @Description: 删除键
+// @receiver i
+// @param pSection 节点名
+// @param pKey 键名
+func (i *Ini) DelKey(pSection string, pKey string)
+
+// Set
+// @Description: 写入配置项
+// @receiver i
+// @param pSection 节点名
+// @param pKey 键名
+// @param pValue 键值
+func (i *Ini) Set(pSection string, pKey string, pValue string)
+
+// Get
+// @Description: 读取配置项
+// @receiver i
+// @param pSection 节点名
+// @param pKey 键名
+// @param pValue 默认值(当配置项不存在时,返回该值)
+// @return string
+// @return bool 配置项是否存在
+func (i *Ini) Get(pSection string, pKey string, pValue string) (string, bool)
+
+// SaveTo
+// @Description: 保存到指定文件
+// @receiver i
+// @param pFileName 文件名
+// @return err
+func (i *Ini) SaveTo(pFileName string) (err error)
+
+// Save
+// @Description: 保存到当前文件
+// @receiver i
+// @return err
+func (i *Ini) Save() (err error)
+```
+
 ### 安装
 
 ```
@@ -13,16 +99,22 @@ go get github.com/gkertop/jini
 	if err != nil {
 		panic(err.Error())
 	}
-	ini.Set("SectionA", "KeyA", "ValueA")
+
 	v1, ok := ini.Get("SectionA", "KeyA", "DefaultValueA")
-	fmt.Printf("A value:%v,has:%v\n", v1, ok)
+	fmt.Printf("v1: value:%v has:%v\n", v1, ok)
+
+	ini.Set("SectionB", "KeyB", "ValueB")
+
 	v2, ok := ini.Get("SectionB", "KeyB", "DefaultValueB")
-	fmt.Printf("B value:%v,has:%v\n", v2, ok)
-	//ini.SaveTo("test.ini")
-	ini.Save()
+	fmt.Printf("v2: value:%v has:%v\n", v2, ok)
+
+	err = ini.Save()
+	if err != nil {
+		panic(err.Error())
+	}
 ```
 
 ```
-A value:ValueA,has:true
-B value:DefaultValueB,has:false
+v1: value:DefaultValueA has:false
+v2: value:ValueB has:true
 ```
--- jini.go
+++ jini.go
@@ -23,6 +23,12 @@ type keyValue struct {
 	value string
 }
 
+// NewIni
+// @Description:加载INI文件
+// @param pFileName 文件名
+// @param pCreate 当文件不存在时,是否创建文件
+// @return rt
+// @return err
 func NewIni(pFileName string, pCreate bool) (rt *Ini, err error) {
 	dataBytes, err := readFile(pFileName, pCreate)
 	if err != nil {
@@ -36,12 +42,11 @@ func NewIni(pFileName string, pCreate bool) (rt *Ini, err error) {
 	dataLines := bytes.Split(dataBytes, []byte("\n"))
 	sectionIndex := 0
 	itemIndex := -1
-	crNum := 0
+	if bytes.HasSuffix(dataLines[0], []byte("\r")) {
+		rt.lineBreak = "\r\n"
+	}
 	for _, line := range dataLines {
 		lineStr := string(line)
-		if strings.HasSuffix(lineStr, "\r") {
-			crNum++
-		}
 		formatStr := strings.TrimSpace(lineStr)
 		if strings.HasPrefix(formatStr, ";") {
 			kv := &keyValue{}
@@ -55,71 +60,125 @@ func NewIni(pFileName string, pCreate bool) (rt *Ini, err error) {
 			rt.section = append(rt.section, s)
 			sectionIndex++
 			itemIndex = -1
-		} else if flagIndex := strings.Index(lineStr, "="); flagIndex > 0 && flagIndex < len(lineStr)-1 {
+		} else if flagIndex := strings.Index(formatStr, "="); flagIndex > 0 && flagIndex < len(formatStr)-1 {
+			flagIndex = strings.Index(lineStr, "=")
 			kv := &keyValue{}
-			kv.key = strings.TrimSpace(lineStr[0:flagIndex])
-			kv.value = lineStr[flagIndex+1:]
+			kv.key = strings.TrimSpace(lineStr[:flagIndex])
+			if strings.HasSuffix(lineStr, "\r") {
+				kv.value = lineStr[flagIndex+1 : len(lineStr)-1]
+			} else {
+				kv.value = lineStr[flagIndex+1:]
+			}
 			rt.section[sectionIndex].item = append(rt.section[sectionIndex].item, kv)
 			itemIndex++
 		} else if itemIndex < 0 {
 			kv := &keyValue{}
 			kv.key = ";;"
-			kv.value = lineStr
+			if strings.HasSuffix(lineStr, "\r") {
+				kv.value = lineStr[:len(lineStr)-1]
+			} else {
+				kv.value = lineStr
+			}
 			rt.section[sectionIndex].item = append(rt.section[sectionIndex].item, kv)
 			itemIndex++
 		} else {
-			rt.section[sectionIndex].item[itemIndex].value += "\n" + lineStr
+			if strings.HasSuffix(lineStr, "\r") {
+				rt.section[sectionIndex].item[itemIndex].value += rt.lineBreak + lineStr[:len(lineStr)-1]
+			} else {
+				rt.section[sectionIndex].item[itemIndex].value += rt.lineBreak + lineStr
+			}
 		}
-	}
-	if crNum > 0 && crNum >= (len(dataLines)-1)/2 {
-		rt.lineBreak = "\r\n"
-	} else {
-		rt.lineBreak = "\n"
 	}
 	return
 }
 
-func readFile(pFileName string, pCreate bool) (rt []byte, err error) {
-	flag := os.O_RDONLY
-	if pCreate {
-		flag = flag | os.O_CREATE
-	}
-	fileObj, err := os.OpenFile(pFileName, flag, 0666)
-	if err != nil {
-		return
-	}
-	defer func(fileObj *os.File) {
-		err := fileObj.Close()
-		if err != nil {
-			panic(err.Error())
-		}
-	}(fileObj)
-	rt, err = ioutil.ReadAll(fileObj)
-	return
-}
-
+// GetFileName
+// @Description: 获取当前文件名
+// @receiver i
+// @return string
 func (i *Ini) GetFileName() string {
 	return i.fileName
 }
 
-func (i *Ini) Set(pSection string, pKey string, pValue string) {
-	s := (*section)(nil)
-	for _, s1 := range i.section {
-		if s1.key == pSection {
-			s = s1
-			break
-		}
+// HasSection
+// @Description: 查询节点是否存在
+// @receiver i
+// @param pSection 节点名
+// @return bool
+func (i *Ini) HasSection(pSection string) bool {
+	_, s := i.findSection(pSection)
+	if s == nil {
+		return false
 	}
+	return true
+}
+
+// HasKey
+// @Description: 查询键是否存在
+// @receiver i
+// @param pSection 节点名
+// @param pKey 键名
+// @return bool
+func (i *Ini) HasKey(pSection string, pKey string) bool {
+	_, s := i.findSection(pSection)
+	if s == nil {
+		return false
+	}
+	_, item := i.findItem(s, pKey)
+	if item == nil {
+		return false
+	}
+	return true
+}
+
+// DelSection
+// @Description: 删除节点
+// @receiver i
+// @param pSection 节点名
+func (i *Ini) DelSection(pSection string) {
+	i1, s := i.findSection(pSection)
+	if s == nil {
+		return
+	}
+	i.section = append(i.section[:i1], i.section[i1+1:]...)
+	return
+}
+
+// DelKey
+// @Description: 删除键
+// @receiver i
+// @param pSection 节点名
+// @param pKey 键名
+func (i *Ini) DelKey(pSection string, pKey string) {
+	i1, s := i.findSection(pSection)
+	if s == nil {
+		return
+	}
+	i2, item := i.findItem(s, pKey)
+	if item == nil {
+		return
+	}
+	i.section[i1].item = append(i.section[i1].item[:i2], i.section[i1].item[i2+1:]...)
+	return
+}
+
+// Set
+// @Description: 写入配置项
+// @receiver i
+// @param pSection 节点名
+// @param pKey 键名
+// @param pValue 键值
+func (i *Ini) Set(pSection string, pKey string, pValue string) {
+	_, s := i.findSection(pSection)
 	if s == nil {
 		s = &section{key: pSection}
 		i.section = append(i.section, s)
 	}
 	if s.item != nil {
-		for _, item := range s.item {
-			if item.key == pKey {
-				item.value = pValue
-				return
-			}
+		_, item := i.findItem(s, pKey)
+		if item != nil {
+			item.value = pValue
+			return
 		}
 	}
 	kv := &keyValue{key: pKey}
@@ -127,22 +186,30 @@ func (i *Ini) Set(pSection string, pKey string, pValue string) {
 	s.item = append(s.item, kv)
 }
 
+// Get
+// @Description: 读取配置项
+// @receiver i
+// @param pSection 节点名
+// @param pKey 键名
+// @param pValue 默认值(当配置项不存在时,返回该值)
+// @return string
+// @return bool 配置项是否存在
 func (i *Ini) Get(pSection string, pKey string, pValue string) (string, bool) {
-	for _, s1 := range i.section {
-		if s1.key == pSection {
-			if s1.item == nil {
-				continue
-			}
-			for _, item := range s1.item {
-				if item.key == pKey {
-					return item.value, true
-				}
-			}
+	_, s := i.findSection(pSection)
+	if s != nil && s.item != nil {
+		_, item := i.findItem(s, pKey)
+		if item != nil {
+			return item.value, true
 		}
 	}
 	return pValue, false
 }
 
+// SaveTo
+// @Description: 保存到指定文件
+// @receiver i
+// @param pFileName 文件名
+// @return err
 func (i *Ini) SaveTo(pFileName string) (err error) {
 	var dataObj strings.Builder
 	for _, s1 := range i.section {
@@ -169,6 +236,10 @@ func (i *Ini) SaveTo(pFileName string) (err error) {
 	return
 }
 
+// Save
+// @Description: 保存到当前文件
+// @receiver i
+// @return err
 func (i *Ini) Save() (err error) {
 	err = i.SaveTo(i.fileName)
 	return
@@ -187,4 +258,41 @@ func (i *Ini) writeFile(pFileName string, pData string) (err error) {
 	}(fileObj)
 	_, err = fileObj.WriteString(pData)
 	return
+}
+
+func readFile(pFileName string, pCreate bool) (rt []byte, err error) {
+	flag := os.O_RDONLY
+	if pCreate {
+		flag = flag | os.O_CREATE
+	}
+	fileObj, err := os.OpenFile(pFileName, flag, 0666)
+	if err != nil {
+		return
+	}
+	defer func(fileObj *os.File) {
+		err := fileObj.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+	}(fileObj)
+	rt, err = ioutil.ReadAll(fileObj)
+	return
+}
+
+func (i *Ini) findSection(pSection string) (int, *section) {
+	for i1, s1 := range i.section {
+		if s1.key == pSection {
+			return i1, s1
+		}
+	}
+	return -1, nil
+}
+
+func (i *Ini) findItem(pSectionObj *section, pKey string) (int, *keyValue) {
+	for i1, item := range pSectionObj.item {
+		if item.key == pKey {
+			return i1, item
+		}
+	}
+	return -1, nil
 }
